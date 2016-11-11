69b1eaa18777e341f5c1799cb5ccaf8fc16d6f45
Waterstrong <sqlin@thoughtworks.com>
2016-11-11 22:19:16 +0800

    [B025] - add gradle dependencies

--- src/main/java/ws/stubby4j/demo/Application.java
+++ src/main/java/ws/stubby4j/demo/Application.java
@@ -1,4 +1,11 @@
 package ws.stubby4j.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }

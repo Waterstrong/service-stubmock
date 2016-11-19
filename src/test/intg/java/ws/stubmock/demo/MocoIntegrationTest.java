685db891d39fa586357cbd272c872a4f770305b8
Waterstrong <sqlin@thoughtworks.com>
2016-11-19 20:54:07 +0800

    [B026] - add moco response json file

--- src/test/intg/java/ws/stubmock/demo/MocoIntegrationTest.java
+++ src/test/intg/java/ws/stubmock/demo/MocoIntegrationTest.java
@@ -1,18 +1,24 @@
 package ws.stubmock.demo;
 
 import static com.github.dreamhead.moco.Moco.httpServer;
+import static com.github.dreamhead.moco.Moco.pathResource;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.springframework.core.io.ClassPathResource;
+import com.github.dreamhead.moco.HttpServer;
 import com.github.dreamhead.moco.Runner;
 
 
 public abstract class MocoIntegrationTest extends BaseIntegrationTest {
-    private static final Runner runner = Runner.runner(httpServer(8882));
-//    Moco.pathResource("books-response.json")
+    private static Runner runner;
 
     @BeforeClass
     public static void startUp() throws Exception {
+        HttpServer server = httpServer(8882);
+        server.response(pathResource(
+                new ClassPathResource("api/json/moco-response.json").getFile().getAbsolutePath()));
+        runner = Runner.runner(server);
         runner.start();
     }
 
